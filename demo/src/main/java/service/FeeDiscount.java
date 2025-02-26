38b006d65015f62cc026bb8784f0599755814ca6
ADMIN <nguyenducmy199x@gmail.com>
2025-02-26 11:58:10 +0700

    commit local

--- demo/src/main/java/service/FeeDiscount.java
+++ demo/src/main/java/service/FeeDiscount.java
@@ -3,6 +3,6 @@ package service;
 import org.json.JSONArray;
 
 public interface FeeDiscount {
-    public JSONArray getFeeAllChannelPayment(String partnerCode, String serviceCodeCol, String serviceProviderCodeCol, String baseAmount);
+//    public JSONArray getFeeAllChannelPayment(String partnerCode, String serviceCodeCol, String serviceProviderCodeCol, String baseAmount);
 
 }
--- demo/src/main/java/service/impl/FeeDiscountImpl.java
+++ demo/src/main/java/service/impl/FeeDiscountImpl.java
@@ -14,39 +14,56 @@ import java.util.Date;
 import java.util.List;
 
 public class FeeDiscountImpl implements FeeDiscount {
+    public static void main(String[] args) {
+        JSONArray jsonArray = getFeeAllChannelPayment("VNPTMONEY", "32", "FT24112001", "50000");
+        System.out.printf("jsonArray = %s\n", jsonArray);
+    }
 
-    @Override
-    public JSONArray getFeeAllChannelPayment(String partnerCode, String serviceCodeCol, String serviceProviderCodeCol, String baseAmount) {
+//    @Override
+    public static JSONArray getFeeAllChannelPayment(String partnerCode, String serviceCodeCol, String serviceProviderCodeCol, String baseAmount) {
         JSONArray jsonArray = new JSONArray();
         List<ColFeeDiscount> feeDiscounts = getFeeDiscounts(partnerCode, serviceCodeCol, serviceProviderCodeCol);
-        feeDiscounts.removeIf(i -> Integer.valueOf(i.getFromValue()) < Integer.valueOf(baseAmount) || Integer.valueOf(i.getFromValue()) > Integer.valueOf(baseAmount)
-                || i.getEnable() == 0 || (i.getStartDate().after((new Date())) && i.getEndDate().before(new Date())));
+
         for (ColFeeDiscount feeDiscount : feeDiscounts) {
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("baseAmount", baseAmount);
             jsonObject.put("channelPaymentCode", feeDiscount.getChannelPaymentCode());
 
-            if(Integer.valueOf(feeDiscount.getMinValue()) < Integer.valueOf(feeDiscount.getValue()) && Integer.valueOf(feeDiscount.getMaxValue()) > Integer.valueOf(feeDiscount.getValue()) ) {
-                jsonObject.put("fee", feeDiscount.getType() == 0 ? feeDiscount.getValue() : 0);
-                jsonObject.put("discount", feeDiscount.getType() == 0 ? 0 : feeDiscount.getValue());
-                jsonObject.put("amount", feeDiscount.getTypeFormula() == 1 ? Integer.valueOf(baseAmount) + Integer.valueOf(feeDiscount.getValue()) : Integer.valueOf(baseAmount) + Integer.valueOf(baseAmount) * Integer.valueOf(feeDiscount.getValue()) / 100);
+            if(feeDiscount.getFromValue() == null && feeDiscount.getToValue() != null && Double.valueOf(feeDiscount.getToValue()) <= Double.valueOf(baseAmount)) {
+                continue;
             }
-            if(Integer.valueOf(feeDiscount.getValue()) == Integer.valueOf(feeDiscount.getMinValue())) {
-                jsonObject.put("fee", feeDiscount.getType() == 0 ? feeDiscount.getMinValue() : 0);
-                jsonObject.put("discount", feeDiscount.getType() == 0 ? 0 : feeDiscount.getMinValue());
-                jsonObject.put("amount", feeDiscount.getTypeFormula() == 1 ? Integer.valueOf(baseAmount) + Integer.valueOf(feeDiscount.getMinValue()) : Integer.valueOf(baseAmount) + Integer.valueOf(baseAmount) * Integer.valueOf(feeDiscount.getMinValue()) / 100);
+            if(feeDiscount.getToValue() == null && feeDiscount.getFromValue() !=null  && Double.valueOf(feeDiscount.getFromValue()) >= Double.valueOf(baseAmount)) {
+                continue;
             }
-            if(Integer.valueOf(feeDiscount.getValue()) == Integer.valueOf(feeDiscount.getMaxValue())) {
-                jsonObject.put("fee", feeDiscount.getType() == 0 ? feeDiscount.getMaxValue() : 0);
-                jsonObject.put("discount", feeDiscount.getType() == 0 ? 0 : feeDiscount.getMaxValue());
-                jsonObject.put("amount", feeDiscount.getTypeFormula() == 1 ? Integer.valueOf(baseAmount) + Integer.valueOf(feeDiscount.getMaxValue()) : Integer.valueOf(baseAmount) + Integer.valueOf(baseAmount) * Integer.valueOf(feeDiscount.getMaxValue()) / 100);
+            if(feeDiscount.getMinValue() == null && feeDiscount.getToValue() != null && Double.valueOf(feeDiscount.getToValue()) <= Double.valueOf(feeDiscount.getValue())) {
+                continue;
+            }
+            if(feeDiscount.getMaxValue() == null && feeDiscount.getMaxValue() != null && Double.valueOf(feeDiscount.getMaxValue()) >= Double.valueOf(feeDiscount.getValue())) {
+                continue;
             }
 
+            if(feeDiscount.getEnable() == 0){
+                continue;
+            }
+            if(feeDiscount.getStartDate().after(new Date())) {
+                continue;
+            }
+            if(feeDiscount.getEndDate().before(new Date())) {
+                continue;
+            }
+
+            if(  (feeDiscount.getFromValue() == null && feeDiscount.getToValue() == null) || feeDiscount.getFromValue() != null && feeDiscount.getToValue() != null && Integer.valueOf(feeDiscount.getFromValue()) < Integer.valueOf(baseAmount) && Integer.valueOf(feeDiscount.getToValue()) > Integer.valueOf(baseAmount)
+            && (feeDiscount.getMinValue() == null && feeDiscount.getMaxValue() == null) || feeDiscount.getMinValue() != null && feeDiscount.getMaxValue() != null && Integer.valueOf(feeDiscount.getMinValue()) < Integer.valueOf(feeDiscount.getValue()) && Integer.valueOf(feeDiscount.getMaxValue()) > Integer.valueOf(feeDiscount.getValue())) {
+                jsonObject.put("fee", feeDiscount.getType() == 0 ? feeDiscount.getValue() : 0);
+                jsonObject.put("discount", feeDiscount.getType() == 0 ? 0 : feeDiscount.getValue());
+                jsonObject.put("amount", feeDiscount.getTypeFormula() == 1 ? Double.valueOf(baseAmount) + Double.valueOf(feeDiscount.getValue()) : Double.valueOf(baseAmount) + Double.valueOf(baseAmount) * Double.valueOf(feeDiscount.getValue()) / 100);
+
+            }
             jsonArray.put(jsonObject);
         }
         return jsonArray;
     }
-    public  List<ColFeeDiscount>  getFeeDiscounts(String partnerCode, String serviceCodeCol, String serviceProviderCodeCol) {
+    public static List<ColFeeDiscount>  getFeeDiscounts(String partnerCode, String serviceCodeCol, String serviceProviderCodeCol) {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
         String sql = "SELECT * FROM SPF_COL_FEE_DISCOUNT_EU where PARTNER_CODE='" + partnerCode + "' AND SEVICE_CODE_COL='" + serviceCodeCol + "' AND SEVICE_PROVIDER_CODE_COL='" + serviceProviderCodeCol + "'";
